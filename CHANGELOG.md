ecb9a983063fdd41867ff8905a37fa46022655c0
YuK1Game <develop@yuk1.jp>
2019-04-05 14:02:36 +0900

    wip

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Change Log
+
+## [0.1] - 2019-04-05
+*First alpha release*
---
+++ tests/yuk1/tests/jobTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace YuK1\Tests;
+
+use \YuK1\Job;
+
+class JobTest extends \PHPUnit\Framework\TestCase
+{
+    public function testTest()
+    {
+        $job = new Job();
+    }
+}
